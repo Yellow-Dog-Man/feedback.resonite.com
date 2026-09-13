8f8be754f4109823ccebb1215e953b9a8f9b521d
ProbablePrime <prime@ahref.co.uk>
2026-09-12 17:34:52 -0700

    write hapiness score to analytics

--- src/backend/routes/api.js
+++ src/backend/routes/api.js
@@ -2,8 +2,13 @@ import { Hono } from 'hono'
 
 export const apiApp = new Hono();
 
+const LANDING = "landing";
+const FEATURE = "feature";
+const BUG = "bug";
+const MODERATION = "moderation";
+
 function getValidFormTypes() {
-  return ['bug', 'landing', 'feature']
+  return [BUG, LANDING, FEATURE]
 }
 
 function redirectTo(location) {
@@ -12,14 +17,14 @@ function redirectTo(location) {
   }
 }
 
-function addProgress(formType, body) {
+function addSuccessMetadata(formType, body) {
   const redirectType = body.type;
-  if (formType === "landing") {
+  if (formType === LANDING) {
     if (body.more === 'no') return {end:true};
 
-    if (redirectType === "bug") return redirectTo("/bug");
-    if (redirectType === "feature") return redirectTo("/feature");
-    if (redirectType === "moderation") return redirectTo("https://moderation.resonite.com");
+    if (redirectType === BUG) return redirectTo("/bug");
+    if (redirectType === FEATURE) return redirectTo("/feature");
+    if (redirectType === MODERATION) return redirectTo("https://moderation.resonite.com");
   }
 }
 
@@ -29,16 +34,43 @@ apiApp.post('/:formType', async (c) => {
     const body = await c.req.parseBody()
     console.log(`Received form post [${formType}]:`, body)
 
+    await processBody(c, formType, body);
+
     return c.json({
       success: true,
       message: `Successfully received submission for ${formType}`,
       receivedAt: new Date().toISOString(),
       formResult: body,
-      ...addProgress(formType, body)
-    })
+      ...addSuccessMetadata(formType, body)
+    });
   } catch (err) {
     console.error(`Error processing form post ${formType}:`, err)
     return c.json({ success: false, error: err.message }, 400)
   }
-})
+});
+
+async function processBody(c, formType, body) {
+  if (formType === LANDING)
+  {
+    const date = new Date().toISOString();
+    c.env.SCORE.writeDataPoint({
+      "doubles": [yesNoToScore(body.happinessScore)],
+      "indexes": [date]
+    });
+    if (body.more ==="yes" && type === "text")
+    {
+
+    }
+  }
+}
+
+function yesNoToScore(yn) {
+  if (yn === "yes")
+    return 1;
+
+  if (yn === "no")
+    return -1;
+
+  return 0;
+}
 
