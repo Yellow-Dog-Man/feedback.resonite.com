8dadac9e2b8cc48dd8d4359df5af62e6792edcf3
ProbablePrime <prime@ahref.co.uk>
2026-09-21 18:58:41 -0700

    remove spaces from key

--- src/backend/services/r2Service.js
+++ src/backend/services/r2Service.js
@@ -1,13 +1,17 @@
 //TODO: config
 const BLOB_DOMAIN = "https://blob.feedback.resonite.com/";
 
+function processKey(key) {
+    return key.replaceAll(" ", "");
+}
+
 export async function uploadFileToR2(bucket, filePrefix, file) {
     if (!file || typeof file === 'string' || !(file instanceof File || file instanceof Blob))
         throw new Error("Invalid file: " + file);
 
     const uniqueId = filePrefix =="" ? crypto.randomUUID() : filePrefix;
     const originalName = file.name || 'attachment';
-    const key = `${new Date().toISOString().split('T')[0]}/${uniqueId}/${originalName}`;
+    const key = processKey(`${new Date().toISOString().split('T')[0]}/${uniqueId}/${originalName}`);
 
     await bucket.put(key, file.stream(), {
         httpMetadata: {
