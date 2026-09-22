59c4ed83502d386f54e51e3624af771cb4ed5ad1
ProbablePrime <prime@ahref.co.uk>
2026-09-21 18:53:16 -0700

    correct urls

--- src/backend/services/r2Service.js
+++ src/backend/services/r2Service.js
@@ -1,10 +1,11 @@
+const BLOB_DOMAIN = "https://blob.feedback.resonite.com/";
 export async function uploadFileToR2(bucket, filePrefix, file) {
     if (!file || typeof file === 'string' || !(file instanceof File || file instanceof Blob))
         throw new Error("Invalid file: " + file);
 
     const uniqueId = filePrefix =="" ? crypto.randomUUID() : filePrefix;
     const originalName = file.name || 'attachment';
-    const key = `${new Date().toISOString().split('T')[0]}/${uniqueId}-${originalName}`;
+    const key = `${new Date().toISOString().split('T')[0]}/${uniqueId}/${originalName}`;
 
     await bucket.put(key, file.stream(), {
         httpMetadata: {
@@ -12,5 +13,5 @@ export async function uploadFileToR2(bucket, filePrefix, file) {
         },
     });
 
-    return key;
+    return BLOB_DOMAIN + key;
 }
