ddce8519828123cc6c4691ad09d237f6aef60ec6
ProbablePrime <prime@ahref.co.uk>
2026-09-22 18:13:55 -0700

    tidy up limits because they were bothering me

--- src/backend/helpers/FormHelpers.ts
+++ src/backend/helpers/FormHelpers.ts
@@ -1,10 +1,11 @@
-export const LANDING:string = "landing";
-export const FEATURE:string = "feature";
-export const BUG:string = "bug";
-export const MODERATION:string = "moderation";
-export const SECURITY:string = "security";
+export const LANDING = "landing";
+export const FEATURE = "feature";
+export const BUG = "bug";
+export const MODERATION = "moderation";
+export const SECURITY = "security";
 
 export const TEXT = "text";
 export const SURVEY = "survey";
 
-export const VALID_FORMS:string[] = [BUG, LANDING, FEATURE, SURVEY];
+export const VALID_FORMS = [BUG, LANDING, FEATURE, SURVEY];
+export const FORM = "form";
--- src/backend/helpers/RateLimits.js
+++ src/backend/helpers/RateLimits.js
@@ -1,5 +1,6 @@
 import { rateLimiter } from "hono-rate-limiter";
 import { WorkersKVStore } from "@hono-rate-limiter/cloudflare";
+import { FORM, LANDING } from "./FormHelpers";
 
 //TODO: we should be using Cloudflares built-in rate limits, but this only supports windows of 10 or 60 seconds right now.
 // This is great for Bots and DDOS, but it is not ok for limits that have longer windows, which... filing a form does.
@@ -14,37 +15,44 @@ import { WorkersKVStore } from "@hono-rate-limiter/cloudflare";
 
 // 4 forms in one Hour.
 export function formLimiter(cx) {
-    return rateLimiter({
-        windowMs: 3_600_000, // 1 Hour
-        limit: isDev(cx.env) ? 100 : 4, // 4 (100 in DEV)
-        keyGenerator: (c) => "form:" + c.req.header("cf-connecting-ip") ?? "",
-        store: new WorkersKVStore({
-            namespace: cx.env.RATE_LIMIT_KV,
-        }),
-    });
+    return createLimiter(cx, keyMaker(FORM), getLimitSettings(FORM));
 }
 
 // 4 landing in one hour
 export function landingLimiter(cx) {
-    return rateLimiter({
+    return createLimiter(cx, keyMaker(LANDING), getLimitSettings(LANDING));
+}
+
+const keyMaker = (key) => {
+    return function keyGenerator(c) {
+        return key + ":" + c.req.header("cf-connecting-ip") ?? ""
+    }
+}
+
+const rateLimitMessage = {
+    "status": 429,
+    "message": "Rate Limit exceeded"
+}
+
+function getLimitSettings(key) {
+    return {
         windowMs: 3_600_000, // 1 Hour
         limit: isDev(cx.env) ? 100 : 4, // 4 (100 in DEV)
-        keyGenerator: (c) => "landing:" + c.req.header("cf-connecting-ip") ?? "",
+    }
+}
+
+function createLimiter(cx, keyGenerator, settings) {
+    return rateLimiter({
+        windowMs: settings.windowMs,
+        limit: settings.limit,
+        keyGenerator: keyGenerator,
+        message: rateLimitMessage,
         store: new WorkersKVStore({
             namespace: cx.env.RATE_LIMIT_KV,
         }),
     });
 }
 
-// This shouldn't change in between environments.
-// I mean it might
-// TODO: rethink this, it was late and I wanted more limits lol
-let cachedDev = null;
 function isDev(env) {
-    if (cachedDev == null)
-    {
-        cachedDev = env.ENVIRONMENT === "development" || env.ENVIRONMENT === "dev" || !env.ENVIRONMENT;
-    }
-    return cachedDev;
+    return env.ENVIRONMENT === "development" || env.ENVIRONMENT === "dev" || !env.ENVIRONMENT;
 }
-
