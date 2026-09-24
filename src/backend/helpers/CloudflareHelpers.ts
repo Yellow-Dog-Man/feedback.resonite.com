8aebe2047e7a93640e3a71ec2717193024c48d28
ProbablePrime <prime@ahref.co.uk>
2026-09-24 16:53:17 -0700

    Update CF helpers to ts

--- src/backend/helpers/CloudflareHelpers.ts
+++ src/backend/helpers/CloudflareHelpers.ts
@@ -1,5 +1,7 @@
+import { Context } from "hono";
+
 export const IP_HEADER = "cf-connecting-ip";
 
-export function GetClientIp(c) {
+export function GetClientIp(c: Context) {
     return c.req.header(IP_HEADER) ?? "";
 }
